b7ee944552626ffd857a12832929e0bbafb6fd2f
Juan Carlos Ayala P <jna.apol@gmail.com>
2025-08-15 12:28:54 -0500

    feat: complete modal to save a new movimento of kardex of product

--- backend/app/Http/Controllers/MovementKardexController.php
+++ backend/app/Http/Controllers/MovementKardexController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MovementKardex;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MovementKardexController extends Controller
 {
-    //
+    public function storeForProduct(Request $request, Product $product)
+    {
+        // return $product;
+        $validated = $request->validate([
+            // 'fecha' => 'required|date',
+            'movement_type' => 'required|in:entrada,salida',
+            'amount' => 'required|numeric|min:0',
+            // 'costo_unitario' => 'required|numeric|min:0',
+            // 'referencia' => 'nullable|string'
+        ]);
+
+        $entry = MovementKardex::create([
+            ...$validated,
+            'product_id' => $product->id,
+            'movement_date' => now()
+        ]);
+
+        return response()->json([
+            'message' => 'Movimiento registrado correctamente',
+            'data' => $entry
+        ], 201);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DailyPartController;
+use App\Http\Controllers\MovementKardexController;
 use App\Models\DailyPart;
 use App\Http\Controllers\OrderSiluciaController;
 use App\Http\Controllers\OrderProductoController;
@@ -8,6 +9,7 @@ use App\Http\Controllers\OrderProductsController;
 use App\Http\Controllers\ProductMovementKardexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
@@ -34,10 +36,10 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     //orders producto routes
     Route::get('/products/{product}/movements-kardex', [ProductMovementKardexController::class, 'index']);
+    Route::post('/products/{product}/kardex',[MovementKardexController::class, 'storeForProduct']);
 });
 
 
     
 
-    
 
