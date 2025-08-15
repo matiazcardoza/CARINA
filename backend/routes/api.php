3f27d31f2e77c49756ff01aab44fdfe52defa95c
matiazcardoza <rohuancaca@est.unap.edu.pe>
2025-08-14 20:04:13 -0500

    :wq

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,47 +20,18 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::put('/daily-work-log/{id}', [DailyPartController::class, 'update']);
     Route::delete('/daily-work-log/{id}', [DailyPartController::class, 'destroy']);
     Route::post('/daily-work-log/complete', [DailyPartController::class, 'completeWork']);
-    // Route::post('/example', function (Request $request) {
-    
-    //     $datos = [
-    //         "valor" => "ejemplo",
-    //         "valordos" => "segundoValor",
-    //         "valorTres" => "tercerValor"
-    //     ];
 
-    //     return response()->json($datos);
-    // });
-});
-
-
-    Route::middleware(['auth:sanctum'])->post('/example', function (Request $request) {
-        $datos = [
-            "valor" => "ejemplo",
-            "valordos" => "segundoValor",
-            "valorTres" => "tercerValor"
-        ];
-
-        return response()->json($datos);
-    });
-
-    // Route::middleware(['auth:sanctum'])->get('/example-return-data', function (Request $request) {
-    Route::get('/example-return-data', function (Request $request) {
-        $datos = [
-            "valor" => "ejemplo",
-            "valordos" => "segundoValor",
-            "valorTres" => "tercerValor"
-        ];
-
-        return response()->json($datos);
-    });
-
+    //orders silucia routes
     Route::get('/orders-silucia', [OrderSiluciaController::class, 'index']);
-    // Route::get('/orders-silucia', [OrderSiluciaController::class, 'index']);
-    // Route::get('/videos/{video}/comments', [CommentController::class, 'index']);
     Route::get('/orders-silucia/{orderSilucia}/products', [OrderSiluciaController::class, 'OrderProductsController']);
     Route::post('/orders-silucia/{orderSilucia}/products', [OrderProductsController::class, 'store']);
 
-    // Route::get('/product/{product}/movement-kardex',[ProductMovementKardex::class, 'index']);
+    //orders producto routes
     Route::get('/products/{product}/movements-kardex', [ProductMovementKardexController::class, 'index']);
+});
 
-            // http://localhost:4200/api/products/1/movement-kardex
+
+    
+
+    
+
