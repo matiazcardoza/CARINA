1b05b9e6810ca2faa4c2f68d8cde132a1e357e52
ROYER MATIAZ HUANCA CARDOZA <rohuancaca@est.unap.edu.pe>
2025-10-08 16:52:00 -0500

    commit of te chabfes diesi

--- backend/resources/views/pdf/daily_part.blade.php
+++ backend/resources/views/pdf/daily_part.blade.php
@@ -640,7 +640,7 @@
                 </tr>
                 <tr>
                     <th style="width: 20%;">PETRÓLEO</th>
-                    <td>{{ $service->fuel_consumed }} Gls. </td>
+                    <td>{{ $dailyPart->sum('initial_fuel') }} Gls. </td>
                     <th style="width: 20%;">GRASA</th>
                     <td>Gls. </td>
                 </tr>
