0281dc19cd93578b46318f3ac2053e6258a8e030
matiazcardoza <rohuancaca@est.unap.edu.pe>
2025-10-16 09:26:50 -0500

    commit of the changes

---
+++ backend/database/seeders/addNigthShift.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class addNigthShift extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('shifts')->insert([
+            [
+                'name' => 'NOCHE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
+}
